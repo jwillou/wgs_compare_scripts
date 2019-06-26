30278c1f034ef5f5aec3a1c79c77aa70706b4831
Janna Willoughby <jwillou@purdue.edu>
2019-06-26 14:47:45 -0400

    update analyses

--- genomesize.R
+++ genomesize.R
@@ -1,13 +1,39 @@
-setwd("/Users/jannawilloughby/GDrive/WGS_divergence/data/")
+setwd("/Users/jannawilloughby/GDrive/WGS_divergence/data/genomesize")
 library(scales)
 library(vioplot)
 library(phytools)
 
 #read in data
-data = read.table("data_june19.csv", header=T, sep=",")
+data = read.table("../data_june19.csv", header=T, sep=",")
 
 ####genome size####
-plot(data$sp1_assembly_length, data$sp2_assembly_length)
-plot(data$sp1_cvalue, data$sp2_cvalue)
-plot(data$sp2_cvalue, data$sp2_assembly_length)
-plot(data$sp1_cvalue, data$sp1_assembly_length)
+data$meanc = apply(cbind(data$sp1_cvalue, data$sp2_cvalue), 1, mean, na.rm=T)
+colors  = c("firebrick2", "dodgerblue2") #
+classes = c("Mammalia",   "Actinopterygii") #  
+sink(file="genomesize_genomic.txt")
+print(summary(lm(gen_K80~meanc, data=data[data$class=="Mammalia",])))
+print(summary(lm(gen_K80~meanc, data=data[data$class=="Actinopterygii" & data$gen_K80 < 0.08,])))
+sink()
+pdf("genomesize_genomic.pdf", height=5.25, width=5)
+plot(-100, -100, xlim=c(-0.1, 5.3), ylim=c(-0.01, 0.17),xlab=NA, ylab="nuclear divergence rate", axes=F)
+axis(side=1, at=seq(0,5,1), labels=T, tick=T, pos=-0.01)
+axis(side=2, at=seq(0,0.15,0.05), labels=T, tick=T, pos=-0.1)
+segments(x0=-0.1, x1=5.2,  y0=-0.01, y1=-0.01)
+segments(x0=-0.1, x1=-0.1, y0=-0.01, y1=0.16)
+segments(x0=-0.1, x1=5.2,  y0=0.16, y1=0.16)
+segments(x0=5.2,  x1=5.2,  y0=-0.01, y1=0.16)
+tlm = lm(gen_K80~meanc, data=data[data$class=="Mammalia",])
+x0=min(data$meanc[data$class=="Mammalia"], na.rm=T)
+x1=max(data$meanc[data$class=="Mammalia"], na.rm=T)
+y0=(tlm$coefficients[2]*min(data$meanc[data$class=="Mammalia"], na.rm=T)) + tlm$coefficients[1]
+y1=(tlm$coefficients[2]*max(data$meanc[data$class=="Mammalia"], na.rm=T)) + tlm$coefficients[1]
+segments(x0=x0, x1=x1, y0=y0, y1=y1, col=colors[1], lwd=1.5)
+points(x=data$meanc[data$class=="Mammalia"], y=data$gen_K80[data$class=="Mammalia"], col=alpha(colors[1], 0.8), pch=19)
+tlm = lm(gen_K80~meanc, data=data[data$class=="Actinopterygii" & data$gen_K80 < 0.08,])
+x0=min(data$meanc[data$class=="Actinopterygii" & data$gen_K80 < 0.08], na.rm=T)
+x1=max(data$meanc[data$class=="Actinopterygii" & data$gen_K80 < 0.08], na.rm=T)
+y0=(tlm$coefficients[2]*min(data$meanc[data$class=="Actinopterygii" & data$gen_K80 < 0.08], na.rm=T)) + tlm$coefficients[1]
+y1=(tlm$coefficients[2]*max(data$meanc[data$class=="Actinopterygii" & data$gen_K80 < 0.08], na.rm=T)) + tlm$coefficients[1]
+segments(x0=x0, x1=x1, y0=y0, y1=y1, col=colors[2], lwd=3)
+points(x=data$meanc[data$class=="Actinopterygii" & data$gen_K80 < 0.08], y=data$gen_K80[data$class=="Actinopterygii" & data$gen_K80 < 0.08], col=alpha(colors[2], 0.8), pch=19)
+dev.off()
---
+++ nuisance.R
@@ -0,0 +1,74 @@
+setwd("/Users/jannawilloughby/GDrive/WGS_divergence/data/nuisance")
+library(scales)
+library(vioplot)
+library(phytools)
+
+#read in data
+data = read.table("../data_june19.csv", header=T, sep=",")####assembly size####
+
+####assembly quality####
+data$assembly = rep(0, nrow(data))
+for(r in 1:nrow(data)){
+  t1 = as.character(data$sp1_assembly[r])
+  t2 = as.character(data$sp2_assembly[r])
+  if(t1=="Chromosome" & t2=="Chromosome"){
+    data$assembly[r] = "CC"
+  }
+  if(t1=="Chromosome" & t2=="Scaffold" | t1=="Scaffold" & t2=="Chromosome" ){
+    data$assembly[r] = "CS"
+  }
+  if(t1=="Scaffold" & t2=="Scaffold"){
+    data$assembly[r] = "SS"
+  }
+  if(t1=="Chromosome" & t2=="Contig" | t1=="Contig" & t2=="Chromosome" ){
+    data$assembly[r] = "Cc"
+  }
+  if(t1=="Contig" & t2=="Scaffold" | t1=="Scaffold" & t2=="Contig" ){
+    data$assembly[r] = "cS"
+  }
+  if(t1=="Contig" & t2=="Contig"){
+    data$assembly[r] = "cc"
+  }
+}
+sink("assemblyqual.txt")
+print(table(data$assembly))
+
+print("CC")
+hist(data$gen_K80[data$assembly=="CC"], breaks=seq(0,0.15, 0.01), xlim=c(0,0.15))
+print(mean(data$gen_K80[data$assembly=="CC"], na.rm=T))
+print(quantile(data$gen_K80[data$assembly=="CC"], probs=c(0.085, 0.915), na.rm=T))
+
+print("CS")
+hist(data$gen_K80[data$assembly=="CS"], breaks=seq(0,0.15, 0.01), xlim=c(0,0.15))
+print(mean(data$gen_K80[data$assembly=="CS"], na.rm=T))
+print(quantile(data$gen_K80[data$assembly=="CS"], probs=c(0.085, 0.915), na.rm=T))
+
+print("SS")
+hist(data$gen_K80[data$assembly=="SS"], breaks=seq(0,0.15, 0.01), xlim=c(0,0.15))
+print(mean(data$gen_K80[data$assembly=="SS"], na.rm=T))
+print(quantile(data$gen_K80[data$assembly=="SS"], probs=c(0.085, 0.915), na.rm=T))
+
+print("cS")
+hist(data$gen_K80[data$assembly=="cS"], breaks=seq(0,0.15, 0.01), xlim=c(0,0.15))
+print(mean(data$gen_K80[data$assembly=="cS"], na.rm=T))
+print(quantile(data$gen_K80[data$assembly=="cS"], probs=c(0.085, 0.915), na.rm=T))
+sink()
+
+####number of chromosomes####
+data$meannc = apply(cbind(data$sp1_chrom_num, data$sp2_chrom_num), 1, mean)
+sink("numberchroms.txt")
+print("all")
+print(summary(lm(gen_K80~meannc, data=data)))
+
+#classes
+classes = c("Mammalia",   "Reptilia",    "Actinopterygii", "Aves")
+for(c in classes){
+  print(c)
+  print(summary(lm(gen_K80~meannc, data=data[data$class==c,])))
+}
+sink()
+
+
+
+
+
